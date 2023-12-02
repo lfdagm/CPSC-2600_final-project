1d7350cdffbf2689d261ed7fb1775be9b48f5ccd
lfdagm <lfdagm@gmail.com>
2023-12-02 04:54:45 -0800

    fixing layouts in signup and login forms, and footer

--- client/src/components/accounts/RoleSelectionPop.js
+++ client/src/components/accounts/RoleSelectionPop.js
@@ -1,21 +1,68 @@
-import { Image, Modal } from 'react-bootstrap';
+import { Image, Modal } from "react-bootstrap";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBCardTitle,
+} from "mdb-react-ui-kit";
 
-export default function SignUp (props) {
-  const newComerImg = require('./../../img/newComer320.jpg');
-  const communityImg = require('./../../img/community320.jpg');
+import{useState} from "react";
+
+import Button from "react-bootstrap/Button";
+
+export default function SignUp(props) {
+  const newComerImg = require("./../../img/newComer320.jpg");
+  const communityImg = require("./../../img/community320.jpg");
+
+
+
 
   return (
     <>
       <Modal show={props.roleShow} onHide={props.handleRoleShowClose} size="lg">
         <Modal.Header closeButton>
-          <Modal.Title>VanComer: Select Your Role:</Modal.Title>
+          <Modal.Title>Select Your Role</Modal.Title>
         </Modal.Header>
-        <Modal.Body >
-        <div className="mb-3 mt-4 .modal-body">
+        <Modal.Body>
+          <MDBRow>
+            <MDBCol>
+              <MDBCard alignment="center">
+                <Button variant="light" onClick={props.handleRoleSelection}>
+                  <MDBCardImage
+                    src={communityImg}
+                    position="top"
+                    alt="community"
+                  />
+                  <MDBCardBody>
+                    <MDBCardTitle>Job Seeker</MDBCardTitle>
+                  </MDBCardBody>
+                </Button>
+              </MDBCard>
+            </MDBCol>
+            <MDBCol>
+              <MDBCard alignment="center">
+                <Button variant="light" onClick={props.handleRoleSelection}>
+                  <MDBCardImage
+                    src={newComerImg}
+                    position="top"
+                    alt="Newcomer"
+                  />
+
+                  <MDBCardBody>
+                    <MDBCardTitle>Newcomer</MDBCardTitle>
+                  </MDBCardBody>
+                </Button>
+              </MDBCard>
+            </MDBCol>
+          </MDBRow>
+
+          {/* <div className="mb-3 mt-4 .modal-body">
           <Image
           className='img-responsive'
           src = {newComerImg}
-          alt='new Comer'
+          alt='newComer'
           onClick={props.handleRoleSelection} 
           fluid rounded
           style={{width:'50%'}}
@@ -27,11 +74,15 @@ export default function SignUp (props) {
           fluid rounded
           style={{width:'50%'}}
           />
-        </div>
+        </div> */}
         </Modal.Body>
+        <Modal.Footer>
+       
+          <Button variant="primary" onClick={props.handleRoleShowClose}>
+            Close
+          </Button>
+        </Modal.Footer>
       </Modal>
     </>
-    
-    
-  )
+  );
 }
--- client/src/components/accounts/SignUp.js
+++ client/src/components/accounts/SignUp.js
@@ -1,10 +1,20 @@
-import { useState, useEffect } from 'react';
-import { Col, Button, Row, Form, Modal } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import { Col, Button, Row, Form, Modal } from "react-bootstrap";
 import axios from "axios";
-import validator from 'validator';
+import validator from "validator";
+
+import {
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBCardFooter,
+  MDBCol,
+  MDBRow,
+  MDBContainer
+ } from "mdb-react-ui-kit";
 
 // For new Client to sign up for the websites
-export default function SignUp (props) {
+export default function SignUp(props) {
   const [firstName, setFirstName] = useState();
   const [lastName, setLastName] = useState();
   const [email, setEmail] = useState();
@@ -15,16 +25,21 @@ export default function SignUp (props) {
   const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
 
   // password Validation??
-  const validate = (value) => { 
-        if (validator.isStrongPassword(value, { 
-            minLength: 10, minLowercase: 1, 
-            minUppercase: 1, minNumbers: 1, minSymbols: 1 
-        })) { 
-            setPasswordErrorMessage('Is Strong Password') 
-        } else { 
-            setPasswordErrorMessage('Is Not Strong Password') 
-        } 
-    } 
+  const validate = (value) => {
+    if (
+      validator.isStrongPassword(value, {
+        minLength: 10,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1,
+      })
+    ) {
+      setPasswordErrorMessage("Is Strong Password");
+    } else {
+      setPasswordErrorMessage("Is Not Strong Password");
+    }
+  };
   // handling submit
   const submitHandling = async (e) => {
     e.preventDefault();
@@ -35,115 +50,142 @@ export default function SignUp (props) {
         lastName: lastName,
         role: role,
         email: email,
-        password: password
-      }
-      await axios.post('http://localhost:3500/api/user/', newUser).then ((repos) => {
-        if (repos.data.result === "success") {
-          const user = {
-            userId: repos.data.userId,
-            firstName: repos.data.firstName,
-            lastName: repos.data.lastName,
-            role: repos.data.role
-          };
-          console.log(user);
-          localStorage.setItem('user', JSON.stringify(user));
-          // setError(false);
-          props.handleSignUpClose();
-          window.location.replace("http://localhost:3000/login");
-        } else {
-          setEmailErrorMessage(true);
-        }
-      })
+        password: password,
+      };
+      await axios
+        .post("http://localhost:3500/api/user/", newUser)
+        .then((repos) => {
+          if (repos.data.result === "success") {
+            const user = {
+              userId: repos.data.userId,
+              firstName: repos.data.firstName,
+              lastName: repos.data.lastName,
+              role: repos.data.role,
+            };
+            console.log(user);
+            localStorage.setItem("user", JSON.stringify(user));
+            // setError(false);
+            props.handleSignUpClose();
+            window.location.replace("http://localhost:3000/login");
+          } else {
+            setEmailErrorMessage(true);
+          }
+        });
     }
   };
   // causing a re-render in the modal
-  useEffect(() => {
-  },[emailErrorMessage]);
+  useEffect(() => {}, [emailErrorMessage]);
 
   return (
     <>
       <Modal show={props.signUpShow} onHide={props.handleSignUpClose}>
         <Modal.Header closeButton>
-          <Modal.Title>VanComer</Modal.Title>
+          <Modal.Title>SignUp</Modal.Title>
         </Modal.Header>
 
-        <Modal.Body>
-          <div className="mb-3 mt-4">
-            <p className=" mb-5">Please enter your details to join us!</p>
-            <Form>
-              <Row className="mb-3">
-                <Form.Group
-                  as={Col}
-                  className="mb-3"
-                  controlId="formFirstName"
-                >
-                  <Form.Label className="text-center">
-                    First Name
-                  </Form.Label>
-                  <Form.Control
+        <Modal.Body className="d-flex flex-column p-3">
+          <MDBContainer className='d-flex flex-column p-2'>
+
+          <MDBRow>
+            <p className=" mb-4">Please enter your details to join us!</p>
+          </MDBRow>
+          <MDBRow>
+          <Form className="p-2">
+            <Row className="mb-3">
+              <Form.Group as={Col} className="mb-3" controlId="formFirstName">
+                <Form.Label className="text-center">First Name</Form.Label>
+                <Form.Control
                   type="text"
                   placeholder="First Name"
-                  onChange={(e)=>setFirstName(e.target.value)}/>
-                </Form.Group>
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </Form.Group>
 
-                <Form.Group
-                  as={Col}
-                  className="mb-3"
-                  controlId="formLastName"
-                >
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Last Name"
-                    onChange={(e)=>setLastName(e.target.value)}
-                  />
-                </Form.Group>
-              </Row>
-              <Form.Group className = "mb-3" controlId="formBasicEmail">
+              <Form.Group as={Col} className="mb-3" controlId="formLastName">
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Last Name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label> Email address</Form.Label>
               <Form.Control
-              type="email"
-              placeholder='johnsmith123@example.com' 
-              onChange={(e)=>setEmail(e.target.value)}/>
-              <Form.Text className = "text-muted">
-                {((emailErrorMessage)? "We will never share your email with anyone else." : "This email already exist.")}
+                type="email"
+                placeholder="johnsmith123@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <Form.Text className="text-muted">
+                {emailErrorMessage
+                  ? "We will never share your email with anyone else."
+                  : "This email already exist."}
               </Form.Text>
-              <Form.Text className = "text-muted">
+              <Form.Text className="text-muted">
                 We will never share your email with anyone else.
               </Form.Text>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPasswordConfirm">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPasswordConfirm">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
                 type="password"
                 placeholder="Enter your Password"
-                onChange={(e)=>{
+                onChange={(e) => {
                   setPassword(e.target.value);
                   validate(e.target.value);
-                }} />
-                {passwordErrorMessage === '' ? null : <>
-                    <span style={{ 
-                        fontWeight: 'bold', 
-                        color: 'red', 
-                    }}>{passwordErrorMessage}</span><br /></>} 
-                <Form.Text className = "text-muted">
-                Your passord should have at least 10 character, with at least 1 uppercase letter, 1 lowercase letter, 1 numberic character, and 1 symbolic character.
+                }}
+              />
+              {passwordErrorMessage === "" ? null : (
+                <>
+                  <span
+                    style={{
+                      fontWeight: "bold",
+                      color: "red",
+                    }}
+                  >
+                    {passwordErrorMessage}
+                  </span>
+                  <br />
+                </>
+              )}
+              <Form.Text className="text-muted">
+                Your passord should have at least 10 character, with at least 1
+                uppercase letter, 1 lowercase letter, 1 numberic character, and
+                1 symbolic character.
               </Form.Text>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Re-enter your Password"
-                onChange={(e)=>setConfirmPassword(e.target.value)} />
-              </Form.Group>
-              <Button variant="primary" type="submit" onClick={submitHandling}>
-                Submit
-              </Button>
-            </Form>
-          </div>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Re-enter your Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+            {/* </Form.Group>
+            <Button variant="primary" type="submit" onClick={submitHandling}>
+              Submit
+            </Button>
+          </Form> */}
+          </Form.Group>
+          </Form>
+          </MDBRow>
+          </MDBContainer>
         </Modal.Body>
+        <Modal.Footer>
+        
+            
+            <Button variant="secondary" onClick={props.handleSignUpClose}>
+            Cancel
+          </Button>
+
+          <Button variant="primary" type="submit" onClick={submitHandling}>
+              Submit
+            </Button>
+        </Modal.Footer>
+          
+       
       </Modal>
     </>
-    
-    
-  )
+  );
 }
